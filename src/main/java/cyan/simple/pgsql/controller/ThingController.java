8b22b110d4d7652e2f2f2c0414644ec4cf2ad5af
cyans <snow22314@outlook.com>
2021-06-10 16:57:25 +0800

    修复

--- src/main/java/cyan/simple/pgsql/controller/ThingController.java
+++ src/main/java/cyan/simple/pgsql/controller/ThingController.java
@@ -6,10 +6,12 @@ import cyan.simple.pgsql.model.RestPage;
 import cyan.simple.pgsql.model.Thing;
 import cyan.simple.pgsql.result.RestResult;
 import cyan.simple.pgsql.service.ThingService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 
 /**
@@ -19,6 +21,7 @@ import java.util.List;
  * @group cyan.tool.kit
  * @date 14:47 2021/5/10
  */
+@Slf4j
 @CrossOrigin
 @RestController
 @RequestMapping("/rest/v0.1.0/pgsql/thing")
@@ -29,30 +32,38 @@ public class ThingController {
     @PostMapping("/create")
     public ResponseEntity create(@RequestBody Thing thing) throws PgsqlException {
         Thing create = thingService.save(thing);
-        return ResponseEntity.ok(RestResult.ok(create));
+        return RestResult.ok(create);
     }
 
     @PostMapping("/update")
     public ResponseEntity update(@RequestBody Thing thing) throws PgsqlException {
         Thing update = thingService.save(thing);
-        return ResponseEntity.ok(RestResult.ok(update));
+        return RestResult.ok(update);
     }
 
     @PostMapping("/save")
     public ResponseEntity save(@RequestBody List<Thing> things) throws PgsqlException {
         List<Thing> save = thingService.saveAll(things);
-        return ResponseEntity.ok(RestResult.ok(save));
+        return RestResult.ok(save);
     }
 
     @DeleteMapping("/delete/{id}")
     public ResponseEntity delete(@PathVariable(name = "id") Long id) throws PgsqlException {
         thingService.deleteById(id);
-        return ResponseEntity.ok(RestResult.ok("删除成功！"));
+        return RestResult.ok("删除成功！");
     }
 
     @PostMapping("/query")
-    public ResponseEntity query(@RequestBody JsonbFilter filter) throws PgsqlException {
+    public ResponseEntity postQuery(@RequestBody JsonbFilter filter) throws PgsqlException {
         RestPage<Thing> thingPage = thingService.queryAllWithFilter(filter);
-        return ResponseEntity.ok(RestResult.ok(thingPage));
+        return RestResult.ok(thingPage);
+    }
+
+    @GetMapping("/query/{id}")
+    public ResponseEntity query(@PathVariable("id") Long id, HttpServletRequest request) throws PgsqlException {
+        String requestToken= request.getHeader("token");
+        log.info("request token: {}", requestToken);
+        Thing thing = thingService.queryById(id);
+        return RestResult.ok(thing);
     }
 }
